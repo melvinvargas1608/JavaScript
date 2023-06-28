e30af18881e71ff665fb5b64b9872ef0c7355c3f
melvinvargas1608 <melvinvargas144@gmail.com>
2023-06-28 16:42:53 -0600

    agregado nuevo archivo, e5_operadoresLogicos.js

---
+++ e5_operadoresLogicos.js
@@ -0,0 +1,40 @@
+//Operadores lógicos - se utilizan normalmente con valores booleanos(lógicos); cuando lo son, devuelven un valor booleano.
+
+//operador AND(&&) - devuelve true si ambas expresiones son verdaderas, de los contario retorna false
+var and1 = true && true; 
+console.log(and1); //true
+
+var and2 = true && false;
+console.log(and2); //flase
+
+var and3 = false && true;
+console.log(and3); //false
+
+var and4 = false && false;
+console.log(and4); //false
+
+console.log('***** Fin operador && *****');
+
+//operador OR(||) - devuelve false si ambas expresiones son flase, de lo contrario retorna true
+var or1 = false || false;
+console.log(or1); //false
+
+var or2 = false || true;
+console.log(or2); //true
+
+var or3 = true || false;
+console.log(or3); //true
+
+var or4 = true || true;
+console.log(or4); //true
+
+console.log('***** Fin operador || *****');
+
+//operador NOT(!) - negación
+var n1 = !true;
+console.log(n1); //false
+
+var n2 = !false;
+console.log(n2); //true
+
+console.log('***** Fin operador ! *****');
