a0a216c38bb3487f8c4a4d85fa3ef1455e829c13
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-01 17:49:34 -0600

    cambios menores en los nombres de los archivos .js

---
+++ dia1_holaMundo.js
@@ -0,0 +1,3 @@
+//Este es el comienzo de los programadores... Hello World!
+console.log('Hello World.!'); //Hello World.!
+
---
+++ dia4_operadoresComparacion.js
@@ -0,0 +1,29 @@
+//Operadores de comparación - devuelven un valor booleano para indicar si la operación es verdadera o falsa.
+
+console.log(7 > 10); //flase
+console.log(1 < 9); //true
+console.log(15 < 10); //flase
+console.log(29 > 20); //true
+console.log('***** Fin operadores de comparación *****'); //true
+
+//Igualdad vs igualdad estricta 
+console.log (3 == 3); //true
+console.log (3 === 3); //true
+console.log (3 == "3"); //true
+console.log (3 === "3"); //false
+console.log (7 == "7"); //true
+console.log(7 === "7"); //false
+console.log('***** Fin operadores vs igualdad estricta *****');
+
+//Asignación (=)
+var miNUmero = 70;
+console.log(miNUmero); //70
+
+//Comparación (==) - compara únicamente el valor
+console.log(9 == 8); //flase
+console.log(5 == '5'); //true
+
+//Igualdad estricta (===) - compara tipo de dato y el valor 
+console.log(10 === '10'); //false
+console.log(10 === 10); //true
+
