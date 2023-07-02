da4b6a9b4d22bdec5c62d29d6d54e20f21079b51
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-01 21:41:02 -0600

    agregado nuevo archivo, dia7_controlFlujo.js

---
+++ dia7_controlFlujo.js
@@ -0,0 +1,23 @@
+//Control de flujo(if/else)
+nombre = 'Marcos';
+var edad = 21;
+var numero = 7;
+var numero2 = -100;
+
+//if
+if (edad > 18) {
+    console.log(nombre,'Eres mayor de edad.'); //Marcos Eres mayor de edad.
+}
+
+//if/else
+if (numero > 0) {
+    console.log('El número es positivo'); //El número es positivo
+} else {
+    console.log('El número es negativo');
+}
+
+if (numero2 > 0) {
+    console.log('El número es positivo');
+} else {
+    console.log('El número es negativo'); //El número es negativo
+}
