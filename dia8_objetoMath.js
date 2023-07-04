48f17dcfbca6cfa3c1852ac052218cdfd29a8d4c
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-03 19:03:53 -0600

    agregado nuevo archivo, dia8_objetoMath.js

---
+++ dia8_objetoMath.js
@@ -0,0 +1,23 @@
+/*Objeto Math - Es un objeto global que ya viene integrado en javaScript y que es de mucha utilidad. El objeto Math tiene varios métodos propios como ser: Math.pow, Math.round, Math.floor, Math.ceil, Math.max, Math.min, Math.random
+*/
+
+//Math.pow - nos permite potenciar un número.
+console.log('Math.pow', Math.pow(2, 5)); //32
+
+//Math.round - redondeará el número decimal al entero más próximo.
+console.log('Math.round', Math.round(1.4)); //1
+
+//Math.floor - redondeará el número decimal al entero de menor valor.
+console.log('Math.floor', Math.floor(5.9)); //5
+
+//Math.ceil - redondeará el número decimal al siguiente entero.
+console.log('Math.ceil', Math.ceil(10.47)); //11
+
+//Math.max - nos permite conocer el valor máximo de un conjunto de números.
+console.log('Valor máximo ', Math.max(10, 19, 16, 11, 15)); //19
+
+//Math.min - nos permite conocer el valor mínimo de un conjunto de números.
+console.log('Valor mínimo ', Math.min(10, 19, 16, 11, 15)); //10
+
+//Math.random - permitirá generar un número aleatorio decimal entre 0 y 1.
+console.log('Número aleatorio ', Math.random()); //0.5698525483045878
