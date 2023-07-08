0a05479473b87c21511590f3d5022934093b6b08
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-07 18:37:02 -0600

    agregado nuevo archivo, dia10_bucleWhile.js

---
+++ dia10_bucleWhile.js
@@ -0,0 +1,8 @@
+//Bucle While - se utiliza cuando no tenemos la certeza de cuantos pasos se van a necesitar para finalizar la ejecusión.
+
+var miNumero = 0;
+
+while (miNumero < 10) {
+    miNumero++;
+    console.log(miNumero); //1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+}
