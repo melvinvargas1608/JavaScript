7730495bacd655f8be8791732b3e9dc8dbb5ac49
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-11 21:46:40 -0600

    agregado nuevo archivo, dia13_arrays.js

---
+++ dia13_arrays.js
@@ -0,0 +1,21 @@
+//Arrays - los arreglos o arrays se conocen dentro de JavaScript como objeto global, estos son colecciones de elementos del mismo tipo.
+
+//Declarando un array
+
+//Ejemplo1
+var frutas = [];
+frutas[0]= 'manzanas'; //asignando valores al arreglo en indice 0
+frutas[1] = 'uvas';
+
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas);
+
+//Ejemplo2
+var animales = ['perro', 'gato', 'caballo', 'vaca'];
+
+console.log(animales);
+
+//Método length - sirve para saber la longitud del arreglo
+console.log(frutas.length);
+console.log(animales.length);
