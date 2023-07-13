854b329009915638a4c4b9631f83000da6348317
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-13 11:55:03 -0600

    agregado nuevos ejercicios de practica

---
+++ ejercicios/e2_variableString.js
@@ -0,0 +1,14 @@
+// Crea una variable "string", puede contener lo que quieras:
+var myString = 'JavaScript';
+
+console.log(myString);
+
+// Crea una variable numérica, puede ser cualquier número:
+var myNumero = 100;
+
+console.log(myNumero); //100
+
+// Crea una variable booleana:
+var myBoolean = true;
+
+console.log(myBoolean); //true
