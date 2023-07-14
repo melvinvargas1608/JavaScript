8f2b3ceed8d5e3626c0ea17fa7620c66c9e700e0
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-14 17:10:43 -0600

    agregado nuevo archivo, e3_funcionDevolverString.js

---
+++ ejercicios/e3_funcionDevolverString.js
@@ -0,0 +1,15 @@
+//Crear una función que retorne un string.
+function devolverString(frase) {
+    return frase;
+}
+
+//Llamado a la función
+console.log(devolverString('Hola JavaScript')); //Hola JavaScript
+
+
+//Función flecha
+/*
+let devolverString = frase => frase;
+
+console.log(devolverString('Hola JavaScript')); //Hola JavaScript
+*/
