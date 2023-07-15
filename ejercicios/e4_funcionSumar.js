81df863ae8b400a4699741eaaca6d705232e4937
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-15 13:38:31 -0600

    agregado nuevo archivo, e4_funcionSumar.js

---
+++ ejercicios/e4_funcionSumar.js
@@ -0,0 +1,15 @@
+//Crear una función que sume dos números.
+function sumarNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+//Llamado a la función
+console.log(sumarNumeros(10, 7)); //17
+
+
+//Función flecha
+/*
+let sumarNumeros = (numero1, numero2) => numero1 + numero2;
+
+console.log(sumarNumeros(30, 15)); //45
+*/
