4b73574fd8e7f4b85bf900edf18ef7fbef6bcf44
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-16 21:08:46 -0600

    agregado nuevo archiov, e5_funcionRestar.js

---
+++ ejercicios/e5_funcionRestar.js
@@ -0,0 +1,15 @@
+//Crear una función que reste dos números.
+function restarNumeros(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+//Llamado a la función
+console.log(restarNumeros(100, 15)); //85
+
+
+//Función flecha
+/*
+let restarNumeros = (numero1, numero2) => numero1 - numero2;
+
+console.log(restarNumeros(75, 15)); //60
+*/
