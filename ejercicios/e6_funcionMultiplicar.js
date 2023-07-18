1b6635b408b0862b0779ca4cd3ac4976c1e01728
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-17 21:22:46 -0600

    agregado nuevo archivo, e6_funcionMultiplicar.js

---
+++ ejercicios/e6_funcionMultiplicar.js
@@ -0,0 +1,15 @@
+//Crear una función que multiplica dos números.
+function multiplicarNumeros(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+//Llamado a la función
+console.log(multiplicarNumeros(10, 7)) //70
+
+
+//Función flecha
+/*
+let multiplicarNumeros = (numero1, numero2) => numero1 * numero2;
+
+console.log(multiplicarNumeros(6, 5)); //30
+*/
