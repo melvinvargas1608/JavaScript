dab2766210206045e57c5197551018638f6662db
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-18 21:36:10 -0600

    agregado nuevo archivo, e7_funcionDividir.js

---
+++ ejercicios/e7_funcionDividir.js
@@ -0,0 +1,15 @@
+//Crear una función que divida dos números
+function dividirNumeros(numero1, numero2) {
+    return numero1 / numero2;
+}
+
+//Llamado a la función
+console.log(dividirNumeros(36, 4)); //9
+
+
+//Función flecha
+/*
+let dividirNumeros = (numero1, numero2) => numero1 / numero2;
+
+console.log(dividirNumeros(75, 5)); //15
+*/
