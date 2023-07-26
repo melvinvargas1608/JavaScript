031282a84614b2341386d589da6009ebe28a6183
melvinvargas1608 <melvinvargas144@gmail.com>
2023-07-26 16:15:47 -0600

    agregado nuevo archivo, e10_funcionMenorNoventa.js

---
+++ ejercicios/e10_funcionMenorNoventa.js
@@ -0,0 +1,24 @@
+// Función que devuelve "true" si el argumento de la función "num" es menor que noventa. De lo contrario, devuelve "false"
+function menorNoventa(numero) {
+    if (numero < 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Llamado de la función
+console.log(menorNoventa(100)); // false
+console.log(menorNoventa(35)); // true
+
+
+//Utilizando función flecha
+/*
+function menorAnoventa(num) {
+    let menor = (num < 90) ? true : false;
+    return menor;
+}
+
+console.log(menorAnoventa(10)); // true
+console.log(menorAnoventa(91)); // false
+*/
